1e48c9c86ae23c7cf8b9e658e90cc716970cefbd
Daniil <dadyarri@gmail.com>
2021-12-10 08:08:08 +0000

    refactor: move config to constant

--- database/core.py
+++ database/core.py
@@ -2,19 +2,19 @@ import os
 
 from tortoise import Tortoise
 
+CONFIG = {
+    "connections": {"default": os.getenv("DATABASE_URL")},
+    "apps": {
+        "models": {
+            "models": [
+                "database.models",
+                "aerich.models",
+            ],
+        },
+    },
+}
+
 
 async def init_db_connection():
-    await Tortoise.init(
-        {
-            "connections": {"default": os.getenv("DATABASE_URL")},
-            "apps": {
-                "models": {
-                    "models": [
-                        "database.models",
-                        "aerich.models",
-                    ],
-                },
-            },
-        }
-    )
+    await Tortoise.init(CONFIG)
     await Tortoise.generate_schemas()
