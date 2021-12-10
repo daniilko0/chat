880b3f22812e196a5efe2e920915dd62e8e63659
Daniil <dadyarri@gmail.com>
2021-12-10 10:38:56 +0000

    feat(server): update running server

--- server/main.py
+++ server/main.py
@@ -26,17 +26,19 @@ async def shutdown(server, app, handler):
 async def create_app() -> web.Application:
     app = web.Application()
     app["wslist"] = {}
-    asyncio.get_event_loop().run_until_complete(init_db_connection())
+    asyncio.ensure_future(init_db_connection())
     app.add_routes(
         [
             web.get("/ws/{slug}", handler=WebSocket, name="ws"),
             web.get("/room/{slug}", handler=ChatRoom, name="get_history"),
-            web.get("/loging", handler=Login, name="login"),
+            web.get("/login", handler=Login, name="login"),
         ]
     )
 
     return app
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    application = create_app()
+    host, port = "127.0.0.1", 8080
+    web.run_app(application, host=host, port=port)
