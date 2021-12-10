79279ebdfd1efab86530b545267ac0e6698bcc2a
Daniil <dadyarri@gmail.com>
2021-12-10 09:23:13 +0000

    feat(database): add some models

--- database/models.py
+++ database/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from tortoise import Model, fields
 
 from database.types import AttachmentType
@@ -13,3 +15,35 @@ class Attachment(Model):
     id = fields.UUIDField(pk=True)
     type = fields.CharEnumField(AttachmentType, max_length=5)
     path = fields.CharField(max_length=50)
+
+
+class Room(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=30)
+
+    def get_all_rooms(self):
+        return self.all()
+
+    def __str__(self):
+        return self.name
+
+
+class Message(Model):
+    id = fields.IntField(pk=True)
+    room = fields.ForeignKeyField("models.Room", related_name="room")
+    user = fields.ForeignKeyField("models.User", related_name="user")
+    text = fields.TextField()
+    attachment = fields.ForeignKeyField("models.Attachment", related_name="attachment")
+    created_at = fields.DatetimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.text
+
+    def as_dict(self):
+        return {
+            "text": self.text,
+            "created_at": self.created_at,
+            "user": self.user,
+            "attachment": self.attachment,
+            "room": self.room,
+        }
